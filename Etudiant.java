1769a50a9799ca8d40140a7d34d056677b08e19b
ATOME Ismael <atome@6-email>
2023-02-20 21:16:02 +0100

    les changements ont ete fait

--- Etudiant.java
+++ Etudiant.java
@@ -1,12 +1,44 @@
 public class Etudiant{
-    int private id;
-    String private nom;
-    String private prenom;
-    int private matricule;
-    public Etudiant(int id; String nom ; String prenom ; int matricule){
+    int  id;
+    String  nom;
+    String  prenom;
+    int matricule;
+    /**
+     * @param id
+     * @param nom
+     * @param prenom
+     * @param matricule
+     */
+    public Etudiant(int id, String nom, String prenom, int matricule) {
         this.id = id;
         this.nom = nom;
         this.prenom = prenom;
         this.matricule = matricule;
     }
-}
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public String getPrenom() {
+        return prenom;
+    }
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+    public int getMatricule() {
+        return matricule;
+    }
+    public void setMatricule(int matricule) {
+        this.matricule = matricule;
+    }
+
+   }
