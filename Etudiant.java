ad979947b5a9634c2be224cd3f81906ca9ff24af
ATOME Ismael <atome@6-email>
2023-02-20 20:45:45 +0100

    add file Etudiant.java

---
+++ Etudiant.java
@@ -0,0 +1,12 @@
+public class Etudiant{
+    int private id;
+    String private nom;
+    String private prenom;
+    int private matricule;
+    public Etudiant(int id; String nom ; String prenom ; int matricule){
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.matricule = matricule;
+    }
+}
